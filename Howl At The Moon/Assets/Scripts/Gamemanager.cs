8daae0aac5c18698ecb345581c5137d33541e4ac
Kevin (Code.Play.Repeat) <code.play.repeat@gmail.com>
2020-11-12 00:52:16 -0800

    Pushing up changes from Matt & Nate that were done on collab. Creating the following folders Environment, State-Management, and Traps in the script folder. This to make it more organized and less out of control.

---
+++ Howl At The Moon/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+   // When Called if the instance is null spawn and assign a new instance.
+    #region Singelton
+    private static Gamemanager _instance;
+    public static Gamemanager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject me = new GameObject("GameManager");
+                me.AddComponent<Gamemanager>();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+
+    private void Awake()
+    {
+        if (_instance == null) _instance = this; //Assign yourself
+        else Destroy(gameObject);
+        //DontDestroyOnLoad(gameObject);
+    }
+
+    public void hello()
+    {
+        print("GM Reporting! " + this.name);
+    }
+
+    public void LoseGame()
+    { print("You Lose"); }
+
+    public void WinGame()
+    { print("You Win"); }
+
+}
