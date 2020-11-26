b0a63d02635820c9cc85bbfb53f809ffcf228fd2
Kevin (Code.Play.Repeat) <code.play.repeat@gmail.com>
2020-11-26 02:18:07 -0800

    Created and hooked up animations for the Manor Lord. Also gave the manor lord the ability to panic on enemy sighting.

---
+++ Howl At The Moon/Assets/Scripts/AI/ManorLordEyesight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Author: Kevin Caton-Largent
+/// Tells the manor lord to freak out
+/// </summary>
+public class ManorLordEyesight : MonoBehaviour
+{
+    private Animator myAnimator;
+    private void Awake()
+    {
+        myAnimator = this.transform.parent.Find("LordGFX").GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            myAnimator.SetBool("Panic", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            myAnimator.SetBool("Panic", false);
+        }
+    }
+}
